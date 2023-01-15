cf1b4bfe90f0d750d4eba3e0071cfadb70634775
Emre EKE <emre.eke@bulutmd.com>
2023-01-15 15:07:31 +0300

    Bot sending message added image.

--- SendChannelMessage.js
+++ SendChannelMessage.js
@@ -1,15 +1,16 @@
 const { Client } = require('discord.js');
 const client = new Client();
+const IMAGE = "https://raw.githubusercontent.com/yemreeke/Discord-Channel-Messaging-Bot/main/logo.png";
 const exampleEmbed = {
   color: 0x0099ff,
   title: "Link",
   url: "https://github.com/yemreeke",
   author: {
     name: "Github Profile",
-    // icon_url: IMAGE_URL,
+    icon_url: IMAGE
   },
   thumbnail: {
-    // url: IMAGE_URL,
+    url: IMAGE,
   },
   fields: [
     {
@@ -19,13 +20,13 @@ const exampleEmbed = {
   timestamp: new Date().toISOString(),
   footer: {
     text: "Github hesabımı takip ederseniz sevinirim.",
-    // icon_url: IMAGE_URL,
+    icon_url: IMAGE,
   },
 };
-client.login('token');
+client.login('token'); // token 'ı kendi tokenınız ile değiştirmeyi unutmayın.
 client.on("ready", async () => {
   client.user.setActivity('🤖 Hello World 🤖', { type: 'PLAYING' });
-  client.channels.fetch('channel_id').then(channel => {
+  client.channels.fetch('kanal_id').then(channel => { // kanal_id'yi mesaj gönderilmesini istediğiniz kanal id ile değiştiriniz.
     channel.send({ embed: exampleEmbed }).then(() => {
       process.exit()
     })
